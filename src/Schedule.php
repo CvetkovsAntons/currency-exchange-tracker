20f2d02c1ad172af3c22c951356ed99cbc07834e
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2025-06-23 13:49:33 +0300

    8 small code improvement

--- src/Schedule.php
+++ src/Schedule.php
@@ -10,7 +10,7 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsSchedule]
-class Schedule implements ScheduleProviderInterface
+readonly class Schedule implements ScheduleProviderInterface
 {
     public function __construct(private CacheInterface $cache) {}
 
@@ -21,4 +21,5 @@ class Schedule implements ScheduleProviderInterface
             ->processOnlyLastMissedRun(true)
             ->add(RecurringMessage::every('1 minute', new ExchangeRateSyncMessage()));
     }
+
 }
