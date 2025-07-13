4e6d7c3c87a5abe1632e6f2bbf5c99a2cae65e19
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2025-07-13 15:56:43 +0300

    28 fixed unit tests from untrack command

--- tests/Command/ExchangeRateStopTrackingCommandTest.php
+++ tests/Command/ExchangeRateStopTrackingCommandTest.php
@@ -3,12 +3,9 @@
 namespace App\Tests\Command;
 
 use App\Command\ExchangeRateStopTrackingCommand;
-use App\Entity\Currency;
-use App\Entity\CurrencyPair;
-use App\Entity\ExchangeRate;
-use App\Enum\Argument;
 use App\Service\Domain\CurrencyPairService;
-use App\Service\Domain\CurrencyService;
+use App\Tests\Internal\Factory\CurrencyPairTestFactory;
+use App\Tests\Internal\Factory\CurrencyTestFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
@@ -16,113 +13,68 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class ExchangeRateStopTrackingCommandTest extends TestCase
 {
-    private CurrencyService&MockObject $currencyService;
     private CurrencyPairService&MockObject $pairService;
     private ExchangeRateStopTrackingCommand $command;
 
     protected function setUp(): void
     {
-        $this->currencyService = $this->createMock(CurrencyService::class);
         $this->pairService = $this->createMock(CurrencyPairService::class);
         $logger = $this->createMock(LoggerInterface::class);
 
-        $this->command = new ExchangeRateStopTrackingCommand(
-            $this->currencyService,
-            $this->pairService,
-            $logger,
-        );
+        $this->command = $this->getMockBuilder(ExchangeRateStopTrackingCommand::class)
+            ->setConstructorArgs([$this->pairService, $logger])
+            ->onlyMethods(['getCurrencyPair'])
+            ->getMock();
     }
 
-    public function testExchangeRateStopTrackingWithArguments(): void
+    public function testSuccess(): void
     {
-        $this->prepareCommand();
+        $from = CurrencyTestFactory::makeEntity('USD');
+        $to = CurrencyTestFactory::makeEntity('EUR');
 
-        $tester = new CommandTester($this->command);
-
-        $tester->execute([
-            Argument::FROM->value => 'USD',
-            Argument::TO->value => 'EUR',
-        ]);
-
-        $output = $tester->getDisplay();
-
-        $this->assertStringContainsString(
-            needle: 'Tracking of USD-EUR exchange rate has been stoped successfully!',
-            haystack: $output
-        );
-        $this->assertSame(0, $tester->getStatusCode());
-    }
-
-    public function testExchangeRateStopTrackingWithoutArguments(): void
-    {
-        $this->prepareCommand();
-
-        $tester = new CommandTester($this->command);
-        $tester->setInputs(['USD', 'EUR']);
-        $tester->execute([]);
-
-        $output = $tester->getDisplay();
-
-        $this->assertStringContainsString(
-            needle: 'Tracking of USD-EUR exchange rate has been stoped successfully!',
-            haystack: $output
-        );
-        $this->assertSame(0, $tester->getStatusCode());
-    }
-
-    public function testExchangeRateStopTrackingWithInvalidCurrency(): void
-    {
-        $this->prepareCommand(fn ($v) => in_array($v, ['USD', 'EUR']));
-
-        $tester = new CommandTester($this->command);
-
-        $tester->setInputs(['XXX', 'USD', 'EUR']);
-
-        $tester->execute([]);
-
-        $output = $tester->getDisplay();
-
-        $this->assertStringContainsString("Currency code XXX isn't valid", $output);
-        $this->assertStringContainsString(
-            needle: 'Tracking of USD-EUR exchange rate has been stoped successfully!',
-            haystack: $output
-        );
-        $this->assertSame(0, $tester->getStatusCode());
-    }
-
-    private function prepareCommand(?callable $isValidCodeReturn = null): void
-    {
-        $from = $this->createMock(Currency::class);
-        $to = $this->createMock(Currency::class);
-        $pair = $this->createMock(CurrencyPair::class);
-        $rate = $this->createMock(ExchangeRate::class);
-
-        $from
-            ->method('getCode')
-            ->willReturn('USD');
-
-        $to
-            ->method('getCode')
-            ->willReturn('EUR');
-
-        if (is_null($isValidCodeReturn)) {
-            $this->currencyService->method('isValidCode')->willReturn(true);
-        } else {
-            $this->currencyService->method('isValidCode')->willReturnCallback($isValidCodeReturn);
-        }
-
-        $this->currencyService
-            ->method('get')
-            ->willReturnMap([['USD', $from], ['EUR', $to]]);
+        $pair = CurrencyPairTestFactory::make($from, $to);
 
         $this->pairService
-            ->method('get')
-            ->with($from, $to)
-            ->willReturn($pair);
+            ->method('getAllTracked')
+            ->willReturn([$pair]);
 
         $this->pairService
+            ->expects($this->once())
             ->method('untrack')
             ->with($pair);
+
+        $tester = new CommandTester($this->command);
+
+        $tester->setInputs(['USD-EUR']);
+
+        $tester->execute([]);
+
+        $output = $tester->getDisplay();
+
+        $this->assertStringContainsString(
+            needle: 'Tracking of USD-EUR exchange rate has been stoped successfully!',
+            haystack: $output
+        );
+        $this->assertSame(0, $tester->getStatusCode());
+    }
+
+    public function testNoTrackingPairsFound(): void
+    {
+        $this->pairService
+            ->method('getAllTracked')
+            ->willReturn([]);
+
+        $tester = new CommandTester($this->command);
+
+        $tester->execute([]);
+
+        $output = $tester->getDisplay();
+
+        $this->assertStringContainsString(
+            "Any tracked currency pair has not been found",
+            $output
+        );
+        $this->assertSame(0, $tester->getStatusCode());
     }
 
 }
